3a1a4cfb5e8c4e6ded28140f54486022e8ff25fc
Zhandos Saparbaev <dosya.saparbai@icloud.com>
2025-04-19 07:37:04 +0500

    Сохранение изменений

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,15 +1,16 @@
 export const sendVerificationCode = async (email) => {
 	const res = await fetch('http://localhost:5000/api/auth/send-code', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({ email }),
+	  method: 'POST',
+	  headers: {
+		'Content-Type': 'application/json',
+	  },
+	  body: JSON.stringify({ email }),
 	});
-
+  
 	if (!res.ok) {
-		throw new Error('Ошибка при отправке кода');
+	  throw new Error('Ошибка при отправке кода');
 	}
-
+  
 	return res.json(); 
-};
+  };
+  
