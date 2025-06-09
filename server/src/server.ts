06a0fedd43013a541557cfe2da3fd3c982c5054a
Dosya02 <dosya.saparbaev@mail.ru>
2025-06-10 03:11:24 +0500

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,6 @@
-import { app } from 'app'
-import { env } from 'config/env'
-import { connectDB } from 'config/database'
+import { app } from './app'
+import { env } from './config/env'
+import { connectDB } from './config/database'
 
 async function start() {
   await connectDB()
