62a392aef82f96a1695bf5fc5932015e4373ed0d
Dosya02 <dosya.saparbaev@mail.ru>
2025-06-10 04:18:47 +0500

    Update index.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,7 +1,14 @@
-export * from './appointments'
-export * from './auth'
-export * from './breeds'
-export * from './pets'
-export * from './services'
-export * from './species'
-export * from './user'
+import express from 'express'
+import { appointmentsRouter, authRouter, breedsRouter, petsRouter, servicesRouter, speciesRouter, userRouter } from './routes'
+
+const app = express()
+
+app.use('/api/appointments', appointmentsRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/breeds', breedsRouter)
+app.use('/api/pets', petsRouter)
+app.use('/api/services', servicesRouter)
+app.use('/api/species', speciesRouter)
+app.use('/api/users', userRouter)
+
+export default app
