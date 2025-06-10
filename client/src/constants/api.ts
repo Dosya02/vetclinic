32738dbfc15163ae423ffa7710693bc94e610198
Dosya02 <dosya.saparbaev@mail.ru>
2025-06-10 06:49:03 +0500

    Update api.ts

--- client/src/constants/api.ts
+++ client/src/constants/api.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'http://localhost:5000/api'
+export const API_BASE_URL =
+	process.env.REACT_APP_API_URL?.replace(/\/$/, '') + '/api'
 
 export const API_ROUTES = {
 	AUTH: {
